f1f4cd557b63fa5413287303e5a8fc50497ed544
5ervant Mark Anthony B. Dungo <5ervant@protonmail.com>
2019-02-08 18:34:58 +0800

    Updated variant breaking changes

--- client/src/containers/Admin/Appearance/Widgets/MarkdownHtml.js
+++ client/src/containers/Admin/Appearance/Widgets/MarkdownHtml.js
@@ -25,9 +25,10 @@ const styles = theme => ({
 });
 
 const variants = [
-  'display4', 'display3', 'display2', 'display1',
-  'headline', 'title', 'subheading',
-  'body2', 'body1', 'caption', 'button',
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+  'subtitle1', 'subtitle2',
+  'body1', 'body2',
+  'button', 'caption', 'overline',
 ];
 const aligns = [ 'inherit', 'left', 'center', 'right', 'justify' ];
 
